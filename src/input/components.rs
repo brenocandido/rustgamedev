fa94e3db159a1e4a408b222cefba9f6dc93bacba
Breno Candido <breno.caandido@gmail.com>
2025-07-22 19:27:27 -0300

    Apply drag separately on each axis

--- src/input/components.rs
+++ src/input/components.rs
@@ -2,6 +2,6 @@ use crate::prelude::*;
 
 /// A vector representing the player's input, accumulated over all frames that ran
 /// since the last time the physics simulation was advanced.
-#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Reflect, InspectorOptions)]
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut, Reflect, InspectorOptions)]
 #[reflect(Component)]
 pub struct AccumulatedInput(pub Vec2);
--- src/physics/systems.rs
+++ src/physics/systems.rs
@@ -1,5 +1,7 @@
-use crate::prelude::*;
+use core::f32;
+
 use crate::physics::*;
+use crate::prelude::*;
 
 /// Advance the physics simulation by one fixed timestep. This may run zero or multiple times per frame.
 ///
@@ -30,31 +32,31 @@ pub fn advance_physics(
         mut acceleration,
     ) in query.iter_mut()
     {
-        // Need to normalize and scale because otherwise
-        // diagonal movement would be faster than horizontal or vertical movement.
+        // Need to normalize and scale because otherwise diagonal movement would be faster than horizontal or vertical
+        // movement.
         // This effectively averages the accumulated input.
-        acceleration.0 = input.0.extend(0.0).normalize_or_zero() * cfg.acceleration;
+        acceleration.0 = input.extend(0.0).normalize_or_zero() * cfg.acceleration;
 
-        if acceleration.0 == Vec3::ZERO {
-            let drag_modulo = cfg.drag * dt;
+        let drag_component = cfg.drag * dt;
 
-            if (drag_modulo * drag_modulo) >= velocity.0.length_squared() {
-                velocity.0 = Vec3::ZERO;
-            } else {
-                let drag_vec = velocity.0.normalize() * drag_modulo;
-                velocity.0 -= drag_vec;
-            }
-        } else {
-            velocity.0 += acceleration.0 * dt;
-            if velocity.0.length_squared() > max_speed_sq {
-                velocity.0 = velocity.normalize_or_zero() * cfg.max_speed;
-            }
+        if acceleration.x < f32::EPSILON {
+            apply_drag_component(&mut velocity.x, drag_component);
+        }
+
+        if acceleration.y < f32::EPSILON {
+            apply_drag_component(&mut velocity.y, drag_component);
+        }
+
+        velocity.0 += acceleration.0 * dt;
+        if velocity.0.length_squared() > max_speed_sq {
+            velocity.0 = velocity.normalize_or_zero() * cfg.max_speed;
         }
 
         previous_physical_translation.0 = current_physical_translation.0;
         current_physical_translation.0 += velocity.0 * dt;
 
-        // Reset the input accumulator, as we are currently consuming all input that happened since the last fixed timestep.
+        // Reset the input accumulator, as we are currently consuming all input that happened since the last fixed
+        // timestep.
         *input = default();
     }
 }
@@ -78,4 +80,16 @@ pub fn interpolate_rendered_transform(
         let rendered_translation = previous.lerp(current, alpha);
         transform.translation = rendered_translation;
     }
+}
+
+//------------------------------------------------------------------------------
+// Auxiliary functionms
+//------------------------------------------------------------------------------
+
+#[inline(always)]
+fn apply_drag_component(v: &mut f32, drag_modulo: f32) {
+    if v.abs() < f32::EPSILON { return; }
+
+    let delta = drag_modulo.min(v.abs());
+    *v -= v.signum() * delta;
 }

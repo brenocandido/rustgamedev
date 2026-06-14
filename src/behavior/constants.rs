776b71b4f96bf86eeacf0318a2d968783b112884
Breno Candido <breno.caandido@gmail.com>
2026-06-14 13:19:07 -0300

    Improve push behavior between entities

--- src/behavior/constants.rs
+++ src/behavior/constants.rs
@@ -1,2 +1,3 @@
 use std::f32::consts::PI;
 pub const WANDER_DEFAULT_BASE_VARIATION: f32 = PI * 0.5;
+pub const DEFAULT_PUSH_WEIGHT: f32 = 2.0;
--- src/behavior/systems.rs
+++ src/behavior/systems.rs
@@ -1,4 +1,4 @@
-use crate::prelude::*;
+use crate::{behavior::DEFAULT_PUSH_WEIGHT, prelude::*};
 use fastrand;
 
 pub fn seek_player_system(
@@ -22,21 +22,40 @@ pub fn seek_player_system(
 }
 
 pub fn flee_from_player_system(
-    player_q: Query<&PhysicalTranslation, With<Player>>,
-    mut enemies: Query<(&PhysicalTranslation, &Flee, &mut AccumulatedInput), Without<Player>>,
+    player_q: Query<(&PhysicalTranslation, &Collider), With<Player>>,
+    mut enemies: Query<
+        (
+            &PhysicalTranslation,
+            &Collider,
+            &Flee,
+            &mut AccumulatedInput,
+        ),
+        Without<Player>,
+    >,
 ) {
-    let Ok(player_tf) = player_q.single() else {
+    let Ok((player_tf, player_col)) = player_q.single() else {
         return;
     };
-    let player_pos = player_tf.truncate();
-    for (tf, flee, mut input) in enemies.iter_mut() {
-        let pos = tf.truncate();
-        let away = pos - player_pos;
-        let distance_sq = flee.distance * flee.distance;
+    let player_pos = &player_tf.truncate();
+    let player_radius = match player_col.0 {
+        ColliderShape::Circle { radius } => radius,
+        _ => 0.0,
+    };
+    for (tf, col, flee, mut input) in enemies.iter_mut() {
+        let radius = match col.0 {
+            ColliderShape::Circle { radius } => radius,
+            _ => 0.0,
+        };
 
-        if away.length_squared() < distance_sq {
-            input.0 += away.normalize() * 1.0; // Accumulate force
-        }
+        let pos = &tf.truncate();
+        input.0 += get_push_vector(
+            pos,
+            player_pos,
+            radius,
+            player_radius,
+            flee.distance,
+            Some(DEFAULT_PUSH_WEIGHT * 2.0),
+        );
     }
 }
 
@@ -130,15 +149,38 @@ pub fn avoid_allies_system(
         };
 
         let pos = tf.truncate();
-        let r = avoid.radius;
+
         for (ally_pos, ally_radius) in &ally_positions {
-            let offset = pos - *ally_pos;
-            let dist = offset.length() - ally_radius - entity_radius;
-            if dist > 0.0 && dist < r {
-                // steer away from this ally, stronger when very close
-                let push_strength = (r - dist) / r; // linear falloff of force
-                input.0 += (offset / dist) * push_strength * 2.0; // AVOID_WEIGHT = 2.0
-            }
+            input.0 += get_push_vector(
+                &pos,
+                ally_pos,
+                entity_radius,
+                *ally_radius,
+                avoid.radius,
+                None,
+            );
         }
     }
+}
+
+fn get_push_vector(
+    self_pos: &Vec2,
+    target_pos: &Vec2,
+    self_radius: f32,
+    target_radius: f32,
+    max_distance: f32,
+    weight: Option<f32>,
+) -> Vec2 {
+    let w: f32 = weight.unwrap_or(DEFAULT_PUSH_WEIGHT);
+
+    let offset = self_pos - target_pos;
+    let dist = (offset.length() - target_radius - self_radius).max(1.0);
+
+    if dist > 0.0 && dist < max_distance {
+        // steer away from target ally, stronger when very close
+        let push_strength = (max_distance - dist) / max_distance; // linear falloff of force
+        return (offset / dist) * push_strength * w;
+    }
+
+    Vec2::ZERO
 }
